9f7321a77aa528ffe15b66a11b555af5e6ad5c37
Brett Hill <bh3zd@mail.missouri.edu>
2014-01-18 02:27:55 -0600

    checkpoint on the steam API work

--- src/main/java/org/poker/irc/messagehandler/DotabuffMessageEventHandler.java
+++ src/main/java/org/poker/irc/messagehandler/DotabuffMessageEventHandler.java
@@ -21,6 +21,12 @@ import java.util.*;
 public class DotabuffMessageEventHandler implements MessageEventHandler {
   private static final Logger LOG = LoggerFactory.getLogger(DotabuffMessageEventHandler.class);
   private Map<String, String> nameToId = Maps.newHashMap();
+  private final String STEAM_API_KEY = System.getenv("STEAM_API_KEY");
+  private enum Result {
+    WIN,
+    LOSS
+  }
+  private Result streakType;
 
   public DotabuffMessageEventHandler() {
     this.populateDefaultPlayers();
@@ -51,12 +57,8 @@ public class DotabuffMessageEventHandler implements MessageEventHandler {
     message = message.toLowerCase();
     if (this.nameToId.containsKey(message)) {
       String playerId = this.nameToId.get(message);
+      Integer currentStreak = getCurrentStreak(playerId);
       String url = "http://dotabuff.com/players/" + playerId;
-      List<Match> recentMatches = new ArrayList<>();
-      recentMatches = getRecentMatches(playerId);
-      for(Match match : recentMatches){
-        LOG.warn(match.getMatch_id().toString());
-      }
       Document document;
       try {
         document = Jsoup.connect(url).get();
@@ -71,10 +73,16 @@ public class DotabuffMessageEventHandler implements MessageEventHandler {
       StringBuilder stringBuilder = new StringBuilder();
       stringBuilder.append("wins: ");
       stringBuilder.append(gamesWon);
-      stringBuilder.append((" | "));
+      stringBuilder.append(" | ");
       stringBuilder.append("losses: ");
       stringBuilder.append(gamesLost);
+      stringBuilder.append(" |  Streak: ");
+      stringBuilder.append(currentStreak.toString());
+      stringBuilder.append(" Game ");
+      stringBuilder.append(streakType.toString());
+      stringBuilder.append(" streak.");
       event.getChannel().send().message(stringBuilder.toString());
+      LOG.warn(currentStreak.toString() + streakType.toString());
     } else {
       event.getChannel().send().message("Unknown player name: " + message);
     }
@@ -96,13 +104,14 @@ public class DotabuffMessageEventHandler implements MessageEventHandler {
     this.nameToId.put("muiy", "78932949");
     this.nameToId.put("dank", "78932949");
   }
+  //API is not giving back the 10 most recent or rather the 10 most recent arent the same as Dotabuff..
   private List<Match> getRecentMatches(String playerId){
-    String STEAM_API_KEY = System.getenv("STEAM_API_KEY");
     List<Match> recentMatches = new ArrayList<Match>();
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
     MatchHistoryResponse matchHistoryResponse;
+    //10 results for now, defaults to 100
     HttpGet httpGet = new HttpGet("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?" +
-                                    "account_id=" + playerId + "&key=" + STEAM_API_KEY );
+                                    "account_id=" + playerId + "&matches_requested=10&key=" + STEAM_API_KEY );
     httpGet.addHeader("Accept", "application/json");
     try (CloseableHttpClient httpClient = HttpClients.createDefault();
          CloseableHttpResponse response = httpClient.execute(httpGet)) {
@@ -117,7 +126,92 @@ public class DotabuffMessageEventHandler implements MessageEventHandler {
     return recentMatches;
   }
 
-  private String getCurrentStreak(String playerId){
-    return null;
+  private List<Result> getRecentResults(Map<String,String> matchesWithPlayerSlot){
+    List<Result> results = new ArrayList<>();
+    Result currentResult;
+    for(Map.Entry<String,String> entry : matchesWithPlayerSlot.entrySet()){
+      currentResult = checkMatchResult(entry.getKey(),entry.getValue());
+      results.add(currentResult);
+    }
+    return results;
+  }
+
+  private Map<String,String> getMatchesWithPlayerSlot(List<Match> matches, String playerId){
+    Map<String,String> matchesWithPlayerSlot = Maps.newHashMap();
+    List<Player> players = new ArrayList<>();
+    String playerSlot = null;
+    for(Match match : matches){
+      players = match.getPlayers();
+      playerSlot = this.getPlayerSlot(players,playerId);
+      matchesWithPlayerSlot.put(match.getMatch_id().toString(), playerSlot);
+    }
+    return matchesWithPlayerSlot;
+  }
+
+  private Result checkMatchResult(String matchId, String playerSlot){
+    Result result = null;
+    boolean radiantWin;
+    List<Player> players = new ArrayList<>();
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    MatchDetailResponse matchDetailResponse ;
+
+    HttpGet httpGet = new HttpGet("https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?" +
+                                  "match_id=" + matchId + "&key=" + STEAM_API_KEY );
+    httpGet.addHeader("Accept", "application/json");
+    try (CloseableHttpClient httpClient = HttpClients.createDefault();
+         CloseableHttpResponse response = httpClient.execute(httpGet)) {
+      HttpEntity httpEntity = response.getEntity();
+      try (Reader reader = new InputStreamReader(httpEntity.getContent())) {
+        matchDetailResponse = gson.fromJson(reader, MatchDetailResponse.class);
+        radiantWin = matchDetailResponse.getResult().getRadiant_win();
+        players =  matchDetailResponse.getResult().getPlayers();
+        for(Player player : players){
+          Integer currentPlayerSlot = player.getPlayer_slot();
+          if(currentPlayerSlot.toString().equals(playerSlot)){
+            if(currentPlayerSlot < 128 && radiantWin || currentPlayerSlot > 4 && !(radiantWin)){
+              result = Result.WIN;
+            }else{
+              result = Result.LOSS;
+            }
+          }
+        }
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return result;
+  }
+//0-4 radiant 128-143 dire
+  private String getPlayerSlot(List<Player> players, String playerId){
+    String playerSlot = null;
+    long playerAccount;
+    for(Player player : players){
+      //?? fml
+      //THIS BUG:  http://stackoverflow.com/questions/20181259/java-gson-parse-not-deserializing-correctly
+      playerAccount = player.getAccount_id().longValue();
+      if (String.valueOf(playerAccount).equals(playerId)){
+        playerSlot = player.getPlayer_slot().toString();
+        break;
+      }
+    }
+    return playerSlot;
+  }
+
+  private int getCurrentStreak(String playerId){
+    int streak = 0;
+    List<Match> recentMatches = this.getRecentMatches(playerId);
+    Map<String,String> matchesWithPlayerSlot = this.getMatchesWithPlayerSlot(recentMatches,playerId);
+    List<Result> recentResults = getRecentResults(matchesWithPlayerSlot);
+    Result prev = recentResults.get(0);
+    for(Result result : recentResults){
+      if(result.equals(prev)){
+        streak++;
+      }else{
+        break;
+      }
+      prev = result;
+    }
+    this.streakType = recentResults.get(0);
+    return streak ;
   }
 }
