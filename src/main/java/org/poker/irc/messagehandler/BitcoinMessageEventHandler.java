6cf41521632ae668785b66f89bb4050c086998c2
Tom B Studee <thomas.studee@gmail.com>
2014-01-18 13:01:35 -0600

    rename mtGox pkg to xeiam

--- src/main/java/org/poker/irc/messagehandler/BitcoinMessageEventHandler.java
+++ src/main/java/org/poker/irc/messagehandler/BitcoinMessageEventHandler.java
@@ -1,26 +1,10 @@
 package org.poker.irc.messagehandler;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.xeiam.xchange.Exchange;
-import com.xeiam.xchange.ExchangeFactory;
-import com.xeiam.xchange.currency.Currencies;
 import com.xeiam.xchange.dto.marketdata.Ticker;
-import com.xeiam.xchange.service.polling.PollingMarketDataService;
-import org.joda.money.*;
-import org.joda.money.format.MoneyAmountStyle;
-import org.joda.money.format.MoneyFormatter;
-import org.joda.money.format.MoneyFormatterBuilder;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.pircbotx.hooks.events.MessageEvent;
 import org.poker.irc.BotUtils;
 import org.poker.irc.MessageEventHandler;
-import org.poker.irc.mtGox.TickerFactory;
 
-import java.io.IOException;
-import java.math.RoundingMode;
 import java.text.*;
 
 public class BitcoinMessageEventHandler implements MessageEventHandler {
@@ -36,7 +20,7 @@ public class BitcoinMessageEventHandler implements MessageEventHandler {
 
   @Override
   public void onMessage(MessageEvent event) {
-    Ticker ticker = org.poker.irc.mtGox.TickerFactory.CreateBtcTicker();
+    Ticker ticker = org.poker.irc.xeiam.TickerFactory.CreateBtcTicker();
     StringBuilder sb = new StringBuilder();
     sb.append(ticker.getTradableIdentifier());
     sb.append(" - last: ");
