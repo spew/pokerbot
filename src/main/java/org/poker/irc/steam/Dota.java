eb98c69c7a5a85e2caaca11323da8efe405d9869
spew <spew@users.noreply.github.com>
2014-01-19 07:25:16 -0800

    Adding util method for fetchign json.

--- src/main/java/org/poker/irc/steam/Dota.java
+++ src/main/java/org/poker/irc/steam/Dota.java
@@ -9,7 +9,9 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.javatuples.Pair;
 import org.poker.irc.Configuration;
+import org.poker.irc.HttpUtils;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -26,40 +28,16 @@ public class Dota {
   //API is not giving back the 10 most recent or rather the 10 most recent arent the same as Dotabuff..
   //TODO: Need to weed out the early abandoned matches which dotabuff excludes..
   public List<Match> getMatches(long playerId, int maxResults) {
-    List<Match> recentMatches = new ArrayList<Match>();
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    MatchHistoryResponse matchHistoryResponse;
-    HttpGet httpGet = new HttpGet("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?" +
-        "account_id=" + playerId + "&matches_requested=" + maxResults + "&key=" + configuration.getSteamApiKey() );
-    httpGet.addHeader("Accept", "application/json");
-    try (CloseableHttpClient httpClient = HttpClients.createDefault();
-         CloseableHttpResponse response = httpClient.execute(httpGet)) {
-      HttpEntity httpEntity = response.getEntity();
-      try (Reader reader = new InputStreamReader(httpEntity.getContent())) {
-        matchHistoryResponse = gson.fromJson(reader, MatchHistoryResponse.class);
-        recentMatches = matchHistoryResponse.getResult().getMatches();
-      }
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-    return recentMatches;
+    String url = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?" +
+        "account_id=" + playerId + "&matches_requested=" + maxResults + "&key=" + configuration.getSteamApiKey();
+    MatchHistoryResponse matchHistoryResponse = HttpUtils.getJson(url, MatchHistoryResponse.class);
+    return matchHistoryResponse.getResult().getMatches();
   }
 
   public MatchDetails getMatchDetails(long matchId) {
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    MatchDetailResponse matchDetailResponse;
-    HttpGet httpGet = new HttpGet("https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?" +
-        "match_id=" + matchId + "&key=" + this.configuration.getSteamApiKey());
-    httpGet.addHeader("Accept", "application/json");
-    try (CloseableHttpClient httpClient = HttpClients.createDefault();
-         CloseableHttpResponse response = httpClient.execute(httpGet)) {
-      HttpEntity httpEntity = response.getEntity();
-      try (Reader reader = new InputStreamReader(httpEntity.getContent())) {
-        matchDetailResponse = gson.fromJson(reader, MatchDetailResponse.class);
-      }
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
+    String url = "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?" +
+        "match_id=" + matchId + "&key=" + this.configuration.getSteamApiKey();
+    MatchDetailResponse matchDetailResponse = HttpUtils.getJson(url, MatchDetailResponse.class);
     return matchDetailResponse.getResult();
   }
 }
