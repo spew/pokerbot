71f3b363041c8b3f21f1ea24ba4b8c9a2830cb32
Tom B Studee <thomas.studee@gmail.com>
2014-01-23 14:07:46 -0600

    convert !doge to use DogecoinAverage

--- src/main/java/org/poker/irc/messagehandler/DogecoinMessageEventHandler.java
+++ src/main/java/org/poker/irc/messagehandler/DogecoinMessageEventHandler.java
@@ -1,11 +1,17 @@
 package org.poker.irc.messagehandler;
 
+import com.xeiam.xchange.dto.marketdata.Ticker;
 import org.apache.commons.lang3.math.*;
+import org.javatuples.Pair;
 import org.pircbotx.hooks.events.MessageEvent;
+import org.poker.irc.BotUtils;
+import org.poker.irc.HttpUtils;
 import org.poker.irc.MessageEventHandler;
-import java.math.BigDecimal;
+import org.poker.irc.dogecoinaverage.DogecoinAverageResponse;
+import org.poker.irc.dogecoinaverage.Market;
 
-import static org.poker.irc.cryptocoincharts.CryptoCoinChart.*;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
 
 public class DogecoinMessageEventHandler implements MessageEventHandler {
 
@@ -30,16 +36,57 @@ public class DogecoinMessageEventHandler implements MessageEventHandler {
       String[] commandParts = message.split(" ");
       switch(commandParts.length){
         case 1:
-          channelResponse = GetCoinInfo("doge");
+          channelResponse = GetDogeInfo();
           break;
         case 2:
           if(NumberUtils.isNumber(commandParts[1])){
             amount = new BigDecimal(commandParts[1]);
-            channelResponse = GetCoinUSDValue("doge", amount);
+            channelResponse = GetDogeUSDValue(amount);
           }
           break;
       }
+
       event.getChannel().send().message(channelResponse);
     }
   }
+
+    String apiUrl = "http://dogecoinaverage.com/BTC.json";
+
+    public String GetDogeInfo(){
+        DogecoinAverageResponse response = HttpUtils.getJson(apiUrl, DogecoinAverageResponse.class,
+                new Pair<>("Accept", "application/json"));
+        StringBuilder sb = new StringBuilder();
+
+        BigDecimal dogePrice = new BigDecimal(response.getVwap());
+        Long volume = 0L;
+
+        for(Market market : response.getMarkets()){
+            volume += Long.parseLong(market.getVolume());
+        }
+
+        BigDecimal dogeVolume = new BigDecimal(volume);
+        BigDecimal btcVolume = dogePrice.multiply(dogeVolume);
+
+        sb.append("DOGE/BTC: ");
+        sb.append(response.getVwap());
+        sb.append(" | vol: ");
+        sb.append(NumberFormat.getNumberInstance().format(btcVolume));
+        sb.append(" BTC | 1000 DOGE = ");
+        Ticker btcTicker = org.poker.irc.xeiam.TickerFactory.CreateBtcTicker();
+        BotUtils.appendMoney(btcTicker.getLast().multipliedBy(dogePrice).multipliedBy(1000), sb);
+        return sb.toString();
+    }
+
+    public String GetDogeUSDValue(BigDecimal amount){
+        DogecoinAverageResponse response = HttpUtils.getJson(apiUrl, DogecoinAverageResponse.class,
+                new Pair<>("Accept", "application/json"));
+
+        StringBuilder sb = new StringBuilder();
+        BigDecimal coinPrice = new BigDecimal(response.getVwap());
+
+        sb.append(String.format("%s DOGE = ", amount.toString()));
+        Ticker btcTicker = org.poker.irc.xeiam.TickerFactory.CreateBtcTicker();
+        BotUtils.appendMoney(btcTicker.getLast().multipliedBy(coinPrice).multipliedBy(amount), sb);
+        return sb.toString();
+    }
 }
