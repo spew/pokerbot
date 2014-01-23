0cc426597f40fa77678c6e3be75832972e465334
Tom B Studee <thomas.studee@gmail.com>
2014-01-23 14:07:46 -0600

    convert !doge to use DogecoinAverage

---
+++ src/main/java/org/poker/irc/dogecoinaverage/DogecoinAverageResponse.java
@@ -0,0 +1,82 @@
+package org.poker.irc.dogecoinaverage;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+
+@Generated("com.googlecode.jsonschema2pojo")
+public class DogecoinAverageResponse {
+
+    @Expose
+    private String currency_name;
+    @Expose
+    private String currency_code;
+    @Expose
+    private List<Market> markets = new ArrayList<Market>();
+    @Expose
+    private String ts;
+    @Expose
+    private String date;
+    @Expose
+    private String vwap;
+    @Expose
+    private List<History> history = new ArrayList<History>();
+
+    public String getCurrency_name() {
+        return currency_name;
+    }
+
+    public void setCurrency_name(String currency_name) {
+        this.currency_name = currency_name;
+    }
+
+    public String getCurrency_code() {
+        return currency_code;
+    }
+
+    public void setCurrency_code(String currency_code) {
+        this.currency_code = currency_code;
+    }
+
+    public List<Market> getMarkets() {
+        return markets;
+    }
+
+    public void setMarkets(List<Market> markets) {
+        this.markets = markets;
+    }
+
+    public String getTs() {
+        return ts;
+    }
+
+    public void setTs(String ts) {
+        this.ts = ts;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getVwap() {
+        return vwap;
+    }
+
+    public void setVwap(String vwap) {
+        this.vwap = vwap;
+    }
+
+    public List<History> getHistory() {
+        return history;
+    }
+
+    public void setHistory(List<History> history) {
+        this.history = history;
+    }
+
+}
---
+++ src/main/java/org/poker/irc/dogecoinaverage/Market.java
@@ -0,0 +1,60 @@
+package org.poker.irc.dogecoinaverage;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+
+@Generated("com.googlecode.jsonschema2pojo")
+public class Market {
+
+    @Expose
+    private String price;
+    @Expose
+    private String volume;
+    @Expose
+    private String ts;
+    @Expose
+    private String market_url;
+    @Expose
+    private String exchange_name;
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public String getTs() {
+        return ts;
+    }
+
+    public void setTs(String ts) {
+        this.ts = ts;
+    }
+
+    public String getMarket_url() {
+        return market_url;
+    }
+
+    public void setMarket_url(String market_url) {
+        this.market_url = market_url;
+    }
+
+    public String getExchange_name() {
+        return exchange_name;
+    }
+
+    public void setExchange_name(String exchange_name) {
+        this.exchange_name = exchange_name;
+    }
+
+}
