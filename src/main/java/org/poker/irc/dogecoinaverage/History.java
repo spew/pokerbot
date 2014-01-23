ad76ac6f7e181a05005cb146a4be33b9dd1277cf
Tom B Studee <thomas.studee@gmail.com>
2014-01-23 14:10:30 -0600

    fix the taaabs omg

--- src/main/java/org/poker/irc/dogecoinaverage/History.java
+++ src/main/java/org/poker/irc/dogecoinaverage/History.java
@@ -6,35 +6,35 @@ import com.google.gson.annotations.Expose;
 @Generated("com.googlecode.jsonschema2pojo")
 public class History {
 
-    @Expose
-    private String vwap;
-    @Expose
-    private String ts;
-    @Expose
-    private String time;
+  @Expose
+  private String vwap;
+  @Expose
+  private String ts;
+  @Expose
+  private String time;
 
-    public String getVwap() {
-        return vwap;
-    }
+  public String getVwap() {
+      return vwap;
+  }
 
-    public void setVwap(String vwap) {
-        this.vwap = vwap;
-    }
+  public void setVwap(String vwap) {
+      this.vwap = vwap;
+  }
 
-    public String getTs() {
-        return ts;
-    }
+  public String getTs() {
+      return ts;
+  }
 
-    public void setTs(String ts) {
-        this.ts = ts;
-    }
+  public void setTs(String ts) {
+      this.ts = ts;
+  }
 
-    public String getTime() {
-        return time;
-    }
+  public String getTime() {
+      return time;
+  }
 
-    public void setTime(String time) {
-        this.time = time;
-    }
+  public void setTime(String time) {
+      this.time = time;
+  }
 
 }
