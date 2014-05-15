446f04caf4848fe5b1292a493818e5ba1c9c1853
spew <spew@users.noreply.github.com>
2014-05-14 21:04:24 -0700

    Adding The Americans.

--- src/main/java/org/poker/irc/scc/SceneBot.java
+++ src/main/java/org/poker/irc/scc/SceneBot.java
@@ -3,6 +3,8 @@ package org.poker.irc.scc;
 import com.google.common.collect.ImmutableSet;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeConstants;
+import org.joda.time.Interval;
+import org.joda.time.Period;
 import org.pircbotx.Channel;
 import org.pircbotx.PircBotX;
 import org.poker.irc.Configuration;
@@ -14,6 +16,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -23,8 +27,8 @@ public class SceneBot {
   private static final Logger LOG = LoggerFactory.getLogger(SceneBot.class);
   private final PircBotX bot;
   private final Configuration configuration;
-  private Set<String> shows = ImmutableSet.of("Mad Men", "Game of Thrones", "Silicon Valley", "Veep");
-  private Map<String, DateTime> showToTimeMap = new HashMap<>();
+  private Map<SceneShow, DateTime> showToTimeMap = new ConcurrentHashMap<>();
+  private Map<SceneShow, DateTime> showToLastCheckTimeMap = new HashMap<>();
 
   public SceneBot(final PircBotX bot, final Configuration configuration) {
     this.bot = bot;
@@ -32,56 +36,57 @@ public class SceneBot {
   }
 
   public void start(final ScheduledExecutorService scheduler, final int delaySeconds) {
-    final Runnable runnable = new Runnable() {
-      @Override
-      public void run() {
-        Credentials credentials = new Credentials(configuration.getSceneAccessUsername(), configuration.getSceneAccessPassword());
-        SceneAccess sceneAccess = new SceneAccess(credentials);
-        DateTime now = DateTime.now();
-        for (String showName : shows) {
-          if (showToTimeMap.containsKey(showName)) {
-            if (showToTimeMap.get(showName).toLocalDateTime().getDayOfWeek() == now.toLocalDateTime().getDayOfWeek()) {
-              continue;
-            }
-          }
-          LOG.info("Looking for show '{}'", showName);
-          List<Torrent> torrents;
-          try {
-            torrents = sceneAccess.findShow(showName);
-          } catch (Exception e) {
-            LOG.warn("Problem obtaining show: " + showName, e);
-            continue;
-          }
-          if (!torrents.isEmpty()) {
-            Torrent torrent = torrents.get(0);
-            if (torrent.getDateAdded().getDayOfMonth() >= now.getDayOfMonth()) {
-              showToTimeMap.put(showName, now);
-              for (Channel channel : bot.getUserBot().getChannels()) {
-                SceneAccessMessageEventHandler.sendTorrent(sceneAccess, channel, torrent);
-              }
-            } else {
-              LOG.info("Torrent was too old for use: url={}, dateAdded={}, now={}", sceneAccess.getUrl() + torrent.getUrl(), torrent.getDateAdded(), now);
-            }
-            sleep(2);
-          }
-        }
-      }
-    };
+    final Credentials credentials = new Credentials(configuration.getSceneAccessUsername(), configuration.getSceneAccessPassword());
+    final SceneAccess sceneAccess = new SceneAccess(credentials);
     Runnable manager = new Runnable() {
       @Override
       public void run() {
         LOG.info("Starting sceneaccess polling manager...");
         while (true) {
-          ScheduledFuture scheduledFuture = scheduler.schedule(runnable, 0, TimeUnit.SECONDS);
-          try {
-            scheduledFuture.get();
-            int sleepTime = getSleepTime();
-            sleep(sleepTime);
-          } catch (ExecutionException e) {
-            LOG.warn("Problem running sceneaccess poller", e);
-          } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+          final DateTime now = DateTime.now();
+          for (final SceneShow show : SceneShow.values()) {
+            if (showToTimeMap.containsKey(show)) {
+              if (showToTimeMap.get(show).toLocalDateTime().getDayOfWeek() == now.toLocalDateTime().getDayOfWeek()) {
+                continue;
+              }
+            }
+            if (showToLastCheckTimeMap.containsKey(show)) {
+              DateTime lastCheckTime = showToLastCheckTimeMap.get(show);
+              int nextCheckTime = show.getWaitDurationSeconds();
+              Interval interval = new Interval(lastCheckTime, now);
+              if (interval.toDurationMillis() < nextCheckTime * 1000) {
+                continue;
+              }
+            }
+            showToLastCheckTimeMap.put(show, now);
+            Runnable findShowRunnable = new Runnable() {
+              @Override
+              public void run() {
+                LOG.info("Looking for show '{}'", show.getName());
+                List<Torrent> torrents;
+                try {
+                  torrents = sceneAccess.findShow(show.getName());
+                } catch (Exception e) {
+                  LOG.warn("Problem obtaining show: " + show.getName(), e);
+                  return;
+                }
+                if (!torrents.isEmpty()) {
+                  Torrent torrent = torrents.get(0);
+                  if (torrent.getDateAdded().getDayOfMonth() >= now.getDayOfMonth()) {
+                    showToTimeMap.put(show, now);
+                    for (Channel channel : bot.getUserBot().getChannels()) {
+                      SceneAccessMessageEventHandler.sendTorrent(sceneAccess, channel, torrent);
+                    }
+                  } else {
+                    LOG.info("Torrent was too old for use: url={}, dateAdded={}, now={}", sceneAccess.getUrl() + torrent.getUrl(), torrent.getDateAdded(), now);
+                  }
+                }
+              }
+            };
+            scheduler.submit(findShowRunnable);
+            sleep(2);
           }
+          sleep(10);
         }
       }
     };
