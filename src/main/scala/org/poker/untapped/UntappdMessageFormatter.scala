9150f1d978c79dd6f6dda43b9e07145e2ec43040
spew <spew@users.noreply.github.org>
2014-08-08 14:46:24 -0700

    Adding brewery name.

--- src/main/scala/org/poker/untapped/UntappdMessageFormatter.scala
+++ src/main/scala/org/poker/untapped/UntappdMessageFormatter.scala
@@ -13,7 +13,7 @@ object UntappdMessageFormatter {
     val venueMessage = if (checkin.venue.isDefined) s"at '${checkin.venue.get.venue_name}' " else ""
     val checkinTime = new DateTime(dateFormatter.parse(checkin.created_at))
     val relativeTime = RelativeTimeFormatter.relativeToNow(checkinTime)
-    s"${checkin.user.user_name} rated '${checkin.beer.beer_name}' ${checkin.rating_score}/5.0 (avg ${rating}) ${venueMessage}${relativeTime}: ${url}"
+    s"${checkin.user.user_name} rated '${checkin.beer.beer_name} (${beer.brewery.brewery_name})' ${checkin.rating_score}/5.0 (avg ${rating}) ${venueMessage}${relativeTime}: ${url}"
   }
 
   private def formatRating(rating: Double) = {
